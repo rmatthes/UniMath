2416f75ef09321a720e5872e6919b3077477bd8a
Ralph Matthes <rmatthes@users.noreply.github.com>
2021-08-17 23:16:38 +0200

    WIP (other files do not compile): now naturality in Ze and X

--- UniMath/SubstitutionSystems/Relativization/LiftingRelativizedContainment.v
+++ UniMath/SubstitutionSystems/Relativization/LiftingRelativizedContainment.v
@@ -14,6 +14,7 @@ Require Import UniMath.CategoryTheory.Core.Functors.
 Require Import UniMath.CategoryTheory.Core.NaturalTransformations.
 Require Import UniMath.CategoryTheory.FunctorCategory.
 Require Import UniMath.CategoryTheory.opp_precat.
+Require Import UniMath.CategoryTheory.PrecategoryBinProduct.
 
 Require Import UniMath.SubstitutionSystems.Relativization.Protomonads.
 Require Import UniMath.SubstitutionSystems.Relativization.DoublyRelativizedRefinedContainment.
@@ -25,43 +26,46 @@ Local Open Scope cat.
 Section lifting_def.
 
   Context {C D E : precategory}.
-  Context (hsD : has_homsets D).
   Context (hs : has_homsets E).
-  Context {J : functor C D}.
-  Context (Ze : precategory_Ptm hsD J).
+  Context (J : functor C D).
+  Context (hsD : has_homsets D).
 
 Let homsetsCE : has_homsets [C, E, hs] := functor_category_has_homsets _ _ hs.
 
   Context (H : functor [C, E, hs] [C, E, hs]).
 
   Definition lifting_of_relativized_containment : UU :=
-    ∏ (T: functor C E), drelrefcont_left_functor hs J (pr1 Ze) T ⟹
-                        functor_composite (functor_opp H) (drelrefcont_left_functor hs J (pr1 Ze) (H T)).
+    ∏ (T: functor C E), functor_composite (functor_opp (pair_functor (functor_ptm_forget hsD J) (functor_identity _)))
+                                          (drelrefcont_functor hs J hsD T) ⟹
+                       functor_composite (functor_opp (pair_functor (functor_ptm_forget hsD J) H))
+                                          (drelrefcont_functor hs J hsD (H T)).
 
-  Definition lifting_of_relativized_containment_op_type : UU :=
-    ∏ (X T: functor C E), drelrefcont_type J (pr1 Ze) X T -> drelrefcont_type J (pr1 Ze) (H X) (H T).
+  Definition lifting_of_relativized_containment_op_type : UU := ∏ (Ze: precategory_Ptm hsD J) (X T: functor C E),
+    drelrefcont_type J (pr1 Ze) X T -> drelrefcont_type J (pr1 Ze) (H X) (H T).
 
   Definition lifting_of_relativized_containment_op:
     lifting_of_relativized_containment -> lifting_of_relativized_containment_op_type.
   Proof.
-    intros lift X T mbind.
-    exact (lift T X mbind).
+    intros lift Ze X T mbind.
+    exact (lift T (Ze,,X) mbind).
   Defined.
 
   Lemma pointwise_naturality_of_lifting (lift: lifting_of_relativized_containment) (T: functor C E)
-        (X X' : [C, E, hs]^op) (α : [C, E, hs]^op ⟦ X, X' ⟧)
-        (mbind : pr1 (drelrefcont_left_functor hs J (pr1 Ze) T X)) (c1 c2 : C) (f : D ⟦ J c1, pr1 Ze c2 ⟧):
-          pr1 (pr1 (lift T) X' (# (drelrefcont_left_functor hs J (pr1 Ze) T) α mbind)) c1 c2 f =
-          drelrefcont_left_functor_on_morphism_op J (pr1 Ze) (H T) (# H α) (pr1 (pr1 (lift T) X mbind)) c1 c2 f.
+        {Ze Ze' : (precategory_Ptm hsD J)^op} (π : (precategory_Ptm hsD J)^op ⟦Ze, Ze'⟧)
+        {X X' : [C, E, hs]^op} (α : [C, E, hs]^op ⟦ X, X' ⟧)
+        (mbind : pr1 (drelrefcont_functor hs J hsD T (pr1 Ze,,X))) (c1 c2 : C) (f : D ⟦ J c1, pr1 Ze' c2 ⟧):
+    pr1 (pr1 (lift T) (Ze',,X') (# (drelrefcont_functor hs J hsD T)
+                                   ((pr1 π,,α):(([C, D, hsD] ⊠ [C, E, hs])^op)⟦(pr1 Ze,,X),(pr1 Ze',,X')⟧) mbind)) c1 c2 f =
+    drelrefcont_functor_on_morphism_op J  (H T) (pr1 π) (# H α) (pr1 (pr1 (lift T) (Ze,,X) mbind)) c1 c2 f.
   Proof.
     generalize f. apply toforallpaths. clear f.
     generalize c2. apply toforallpaths. clear c2.
     generalize c1. apply toforallpaths. clear c1.
-    assert (aux :=  pr2 (lift T) X X' α).
+    assert (aux :=  pr2 (lift T) (Ze,,X) (Ze',,X') (π,,α)).
     apply toforallpaths in aux.
     assert (auxinst := maponpaths pr1 (aux mbind)).
     exact auxinst.
-Qed.
+  Qed.
 
 End lifting_def.
 
@@ -71,20 +75,19 @@ Section Examples.
 
   Context {C : precategory}.
   Context (hs : has_homsets C).
-  Context (Ze : precategory_Ptm hs (functor_identity C)).
 
   Local Notation "'EndC'":= ([C, C, hs]) .
 
   (** first try the operation only *)
-  Definition lifting_of_flat_op_op (X T: functor C C) (mbind: drelrefcont_type (functor_identity C) (pr1 Ze) X T) :
+  Definition lifting_of_flat_op_op (Ze : precategory_Ptm hs (functor_identity C)) (X T: functor C C) (mbind: drelrefcont_type (functor_identity C) (pr1 Ze) X T) :
     drelrefcont_op (functor_identity C) (pr1 Ze) (Flat_H C hs X) (Flat_H C hs T).
   Proof.
     intros c1 c2 f.
     exact (pr1 mbind (X c1) (T c2) (pr1 mbind c1 c2 f · (pr2 Ze) (T c2))). (** we need protomonads! *)
   Defined.
 
-  Lemma lifting_of_flat_op_ok (X T: functor C C) (mbind: drelrefcont_type (functor_identity C) (pr1 Ze) X T) :
-    drelrefcont_natural (lifting_of_flat_op_op X T mbind).
+  Lemma lifting_of_flat_op_ok (Ze : precategory_Ptm hs (functor_identity C)) (X T: functor C C) (mbind: drelrefcont_type (functor_identity C) (pr1 Ze) X T) :
+    drelrefcont_natural (lifting_of_flat_op_op Ze X T mbind).
   Proof.
     intros c1 c1' c2 c2' h1 h2 f.
     cbn.
@@ -107,37 +110,38 @@ Section Examples.
     apply idpath.
   Qed.
 
-  Definition lifting_of_flat_op: lifting_of_relativized_containment_op_type hs hs Ze (Flat_H C hs).
+  Definition lifting_of_flat_op: lifting_of_relativized_containment_op_type hs (functor_identity C) hs (Flat_H C hs).
   Proof.
-    intros X T mbind.
-    exists (lifting_of_flat_op_op X T mbind).
-    exact (lifting_of_flat_op_ok X T mbind).
+    intros Ze X T mbind.
+    exists (lifting_of_flat_op_op Ze X T mbind).
+    exact (lifting_of_flat_op_ok Ze X T mbind).
   Defined.
 
-  (* now with naturality in [X]: *)
-  Definition lifting_of_flat: lifting_of_relativized_containment hs hs Ze (Flat_H C hs).
+  (* now with naturality in [Ze] and [X]: *)
+  Definition lifting_of_flat: lifting_of_relativized_containment hs (functor_identity C) hs (Flat_H C hs).
   Proof.
     intro T.
     use make_nat_trans.
-    - intros X mbind.
-      cbn in X.
+    - intros ZeX mbind.
+      cbn in ZeX.
       cbn.
       apply lifting_of_flat_op.
       cbn in mbind.
       exact mbind.
-    - intros X X' α.
-      cbn in X, X', α.
+    - intros ZeX ZeX' πα.
+      cbn in ZeX, ZeX', πα.
+      induction ZeX as [Ze X]. induction ZeX' as [Ze' X']. induction πα as [π α].
       (* show_id_type. *)
       apply funextfun. intro mbind.
       apply (drelrefcont_type_eq hs).
       intros c1 c2 f.
       cbn.
-      unfold drelrefcont_left_functor_on_morphism_op, lifting_of_flat_op_op, Flat_H_mor, HorizontalComposition.horcomp.
+      unfold drelrefcont_functor_on_morphism_op, lifting_of_flat_op_op, Flat_H_mor, HorizontalComposition.horcomp.
       cbn.
       repeat rewrite <- assoc.
       apply maponpaths.
       cbn in mbind.
-      assert (aux := pr2 mbind _ _ _ _ (α c1) (identity (T c2)) (pr1 mbind c1 c2 f · pr2 Ze (T c2))).
+      assert (aux := pr2 mbind _ _ _ _ (pr1 α c1) (identity (T c2)) (pr1 mbind c1 c2 (f · pr1 π c2) · pr2 Ze (T c2))).
       rewrite functor_id in aux.
       rewrite id_right in aux.
       eapply pathscomp0.
@@ -146,7 +150,8 @@ Section Examples.
       }
       rewrite functor_id.
       rewrite id_right.
-      apply idpath.
+      do 3 apply maponpaths.
+      apply (pr2 π).
   Defined.
 
 End Examples.
