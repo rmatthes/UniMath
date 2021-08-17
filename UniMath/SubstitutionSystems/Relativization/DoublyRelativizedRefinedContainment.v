3280fe10f4b49ba258003fc62252084be103c7a1
Ralph Matthes <Ralph.Matthes@irit.fr>
2021-08-17 19:04:45 +0200

    WIP (the other files do not compile): rework functor into bifunctor also depending on Z + new notion containment
    formally, Z is now a protomonad Ze, but this is only used for containment - will be changed in a short while

--- UniMath/SubstitutionSystems/Relativization/DoublyRelativizedRefinedContainment.v
+++ UniMath/SubstitutionSystems/Relativization/DoublyRelativizedRefinedContainment.v
@@ -17,17 +17,66 @@ Require Import UniMath.CategoryTheory.Core.NaturalTransformations.
 Require Import UniMath.CategoryTheory.FunctorCategory.
 Require Import UniMath.CategoryTheory.categories.HSET.Core.
 Require Import UniMath.CategoryTheory.opp_precat.
+Require Import UniMath.CategoryTheory.PrecategoryBinProduct.
 
 Require Import UniMath.SubstitutionSystems.Relativization.Protomonads.
 
 Local Open Scope cat.
 
+Notation "A ⊠ B" := (precategory_binproduct A B) (at level 38).
+
+Section containment_def.
+
+  Context {C E : precategory}.
+  Context (hs : has_homsets E).
+  Context (X T : functor C E).
+
+  Definition containment_op : UU := ∏ (c1 c2: C), C⟦c1, c2⟧ -> E⟦X c1, T c2⟧.
+
+  Definition containment_natural (leq: containment_op) : UU :=
+    forall (c1 c1' c2 c2': C) (h1: c1' --> c1) (h2: c2 --> c2') (f: c1 --> c2),
+      #X h1 · (leq c1 c2 f) · #T h2 = leq c1' c2' (h1 · f · h2).
+
+  Lemma isaprop_containment_natural (leq: containment_op): isaprop (containment_natural leq).
+  Proof.
+    repeat (apply impred; intros).
+    apply hs.
+  Qed.
+
+  Definition containment_type : UU := ∑ (leq: containment_op), containment_natural leq.
+
+  Lemma containment_type_eq (leq leq': containment_type):
+    (forall (c1 c2: C) (f: c1 --> c2), pr1 leq c1 c2 f = pr1 leq' c1 c2 f) -> leq = leq'.
+  Proof.
+    intro Hyp.
+    assert (Hyp' : pr1 leq = pr1 leq').
+    { apply funextsec; intro c1. apply funextsec; intro c2. apply funextsec; intro f.
+      apply Hyp.
+    }
+    apply (total2_paths_f Hyp'), proofirrelevance, isaprop_containment_natural.
+  Qed.
+
+  Lemma isaset_containment_type: isaset containment_type.
+  Proof.
+    apply (isofhleveltotal2 2).
+    - apply impred; intro c1. apply impred; intro c2. apply impred; intro f. apply hs.
+    - intro leq. apply isasetaprop, isaprop_containment_natural.
+  Qed.
+
+  Definition containment: hSet := containment_type ,, isaset_containment_type.
+
+End containment_def.
+
 Section drelrefcont_def.
 
   Context {C D E : precategory}.
   Context (hs : has_homsets E).
-  Context (J Z : functor C D).
+  Context (J : functor C D).
+  Context (hsD : has_homsets D).
+  Context (Ze : precategory_Ptm hsD J).
   Context (X T : functor C E).
+
+  Let Z : functor C D := pr1 Ze.
 
   Definition drelrefcont_op : UU := ∏ (c1 c2: C), D⟦J c1, Z c2⟧ -> E⟦X c1, T c2⟧.
   Definition drelrefcont_natural (mbind: drelrefcont_op) : UU :=
@@ -61,72 +110,103 @@ Section drelrefcont_def.
 
   Definition drelrefcont: hSet := drelrefcont_type ,, isaset_drelrefcont_type.
 
+  Definition leq_from_mbind (mbind: drelrefcont_type): containment_type X T.
+  Proof.
+    use tpair.
+    - intros c1 c2 f.
+      exact (pr1 mbind c1 c2 (#J f · pr2 Ze c2)).
+    - intros c1 c1' c2 c2' h1 h2 f.
+      eapply pathscomp0.
+      { apply (pr2 mbind). }
+      apply maponpaths.
+      do 2 rewrite functor_comp.
+      repeat rewrite <- assoc.
+      do 2 apply maponpaths.
+      apply pathsinv0.
+      apply nat_trans_ax.
+  Defined.
+  (** comment: this is the only construction that exploits that we have a protomonad; it could have
+      been done under this extra assumption, but for the extensions to come, it looked more natural to
+      have it generally *)
+
 End drelrefcont_def.
 
-Arguments drelrefcont_natural {_ _ _ _ _ _ _} _.
+Arguments drelrefcont_natural {_ _ _ _ _ _ _ _} _.
 
-Section drelrefcont_dependent_on_X.
+Section drelrefcont_dependent_on_Ze_and_X.
 
   Context {C D E : precategory}.
   Context (hs : has_homsets E).
-  Context (J Z : functor C D).
+  Context (J : functor C D).
+  Context (hsD : has_homsets D).
   Context (T : functor C E).
 
-  Definition drelrefcont_left_functor_on_morphism_op {X X': functor C E} (α: X' ⟹ X) (mbind: drelrefcont_op J Z X T): drelrefcont_op J Z X' T.
+  Definition drelrefcont_functor_on_morphism_op {Ze Ze': precategory_Ptm hsD J} (π: Ze' --> Ze)
+             {X X': functor C E} (α: X' ⟹ X) (mbind: drelrefcont_op J hsD Ze X T): drelrefcont_op J hsD Ze' X' T.
   Proof.
     intros c1 c2 f.
-    exact (α c1 · mbind c1 c2 f).
+    exact (α c1 · mbind c1 c2 (f · pr1 π c2)).
   Defined.
 
-  Lemma drelrefcont_left_functor_on_morphism_op_ok (X X': functor C E) (α: X' ⟹ X) (mbind: drelrefcont_op J Z X T):
-    drelrefcont_natural mbind -> drelrefcont_natural (drelrefcont_left_functor_on_morphism_op α mbind).
+  Lemma drelrefcont_functor_on_morphism_op_ok {Ze Ze': precategory_Ptm hsD J} (π: Ze' --> Ze)
+        {X X': functor C E} (α: X' ⟹ X) (mbind: drelrefcont_op J hsD Ze X T):
+    drelrefcont_natural mbind -> drelrefcont_natural (drelrefcont_functor_on_morphism_op π α mbind).
   Proof.
     intro Hyp.
     intros c1 c1' c2 c2' h1 h2 f.
-    unfold drelrefcont_left_functor_on_morphism_op.
+    unfold drelrefcont_functor_on_morphism_op.
     rewrite assoc.
     rewrite nat_trans_ax.
     eapply pathscomp0.
     { do 2 rewrite <- assoc. apply maponpaths. rewrite assoc. apply Hyp. }
-    apply idpath.
+    do 2 apply maponpaths.
+    repeat rewrite <- assoc.
+    do 2 apply maponpaths.
+    apply pathsinv0.
+    apply nat_trans_ax.
   Qed.
 
-  Definition drelrefcont_left_functor_data: functor_data [C, E, hs]^op HSET.
+  Definition drelrefcont_functor_data: functor_data (precategory_Ptm hsD J ⊠ [C, E, hs])^op HSET.
   Proof.
     use make_functor_data.
-    - intro X. apply (drelrefcont hs J Z X T).
-    - intros X X' α mbind. cbn in α.
-      exists (drelrefcont_left_functor_on_morphism_op α (pr1 mbind)).
-      apply drelrefcont_left_functor_on_morphism_op_ok.
+    - intro ZeX. apply (drelrefcont hs J hsD (pr1 ZeX) (pr2 ZeX) T).
+    - intros ZeX ZeX' πα mbind. induction ZeX as [Ze X]. induction ZeX' as [Ze' X']. induction πα as [π α]. cbn in π, α.
+      exists (drelrefcont_functor_on_morphism_op π α (pr1 mbind)).
+      apply drelrefcont_functor_on_morphism_op_ok.
       exact (pr2 mbind).
   Defined.
 
-  Lemma is_functor_drelrefcont_left_functor_data: is_functor drelrefcont_left_functor_data.
+  Lemma is_functor_drelrefcont_functor_data: is_functor drelrefcont_functor_data.
   Proof.
     split.
-    - intro X.
+    - intro ZeX. induction ZeX as [Ze X].
       apply funextfun. intro mbind.
       (* show_id_type. *)
       apply (drelrefcont_type_eq hs).
       intros c1 c2 f.
       cbn.
-      unfold drelrefcont_left_functor_on_morphism_op.
+      unfold drelrefcont_functor_on_morphism_op.
+      rewrite id_right.
       apply id_left.
-    - intros X X' X'' α α'.
+    - intros ZeX ZeX' ZeX'' πα πα'.
       apply funextfun. intro mbind.
       apply (drelrefcont_type_eq hs).
       intros c1 c2 f.
       cbn.
-      unfold drelrefcont_left_functor_on_morphism_op.
+      unfold drelrefcont_functor_on_morphism_op.
       rewrite assoc.
+      do 2 apply maponpaths.
+      rewrite <- assoc.
       apply idpath.
   Qed.
 
-  Definition drelrefcont_left_functor: functor [C, E, hs]^op HSET.
+  Definition drelrefcont_functor: functor (precategory_Ptm hsD J ⊠ [C, E, hs])^op HSET.
   Proof.
     use make_functor.
-    - exact drelrefcont_left_functor_data.
-    - exact is_functor_drelrefcont_left_functor_data.
+    - exact drelrefcont_functor_data.
+    - exact is_functor_drelrefcont_functor_data.
   Defined.
 
-End drelrefcont_dependent_on_X.
+  (** pointedness does not enter this construction *)
+
+End drelrefcont_dependent_on_Ze_and_X.
